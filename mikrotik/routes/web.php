a7996714d1f20851bfcfe2cd4ae3c473456a1b6a
BagasRiswansyah <muhammabagas278@gmial.com>
2025-09-09 20:51:43 +0700

    perubahan title

--- mikrotik/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController;
-
-Route::get('/', [ItemController::class, 'login'])->name('login');
-Route::resource('items', ItemController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', [ItemController::class, 'dashboard'])->name('dashboard');
-Route::resource('items', ItemController::class);
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
